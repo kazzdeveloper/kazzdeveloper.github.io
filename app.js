610f8b469ed1ed6541ac28ebbef86c7d88ea6161
kazzdev <tjixtx@gmail.com>
2024-06-03 23:30:24 +0200

    Feel of website

--- app.js
+++ app.js
@@ -12,3 +12,48 @@ function calculateAge(birthdate) {
 }
 const birthdate = '2009-03-21';
 document.getElementById('age').textContent = calculateAge(birthdate);
+
+const ball = document.querySelector("div#ball");
+
+let mouseX = 0;
+let mouseY = 0;
+
+let ballX = 0;
+let ballY = 0;
+
+let speed = 0.25;
+
+
+function animate(){
+  
+  let distX = mouseX - ballX;
+  let distY = mouseY - ballY;
+  
+  
+  ballX = ballX + (distX * speed);
+  ballY = ballY + (distY * speed);
+  
+  ball.style.left = ballX + "px";
+  ball.style.top = ballY + "px";
+  
+  requestAnimationFrame(animate);
+}
+animate();
+
+document.addEventListener("mousemove", function(event){
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+})
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show");
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
