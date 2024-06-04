d201262a89c56d618d558723c8eefdf11a981968
kazzdev <tjixtx@gmail.com>
2024-06-04 21:16:02 +0200

    Redesign

--- app.js
+++ app.js
@@ -13,32 +13,37 @@ function calculateAge(birthdate) {
 const birthdate = '2009-03-21';
 document.getElementById('age').textContent = calculateAge(birthdate);
 
-const ball = document.querySelector("div#ball");
-
+const outerCircle = document.querySelector("div#OuterCircleCursor");
 let mouseX = 0;
 let mouseY = 0;
+let outerCircleX = 0;
+let outerCircleY = 0;
+let outerCircleSpeed = 0.12;
+function animateOuterCircle(){
+  let outerCircledDistX = mouseX - outerCircleX;
+  let outerCircledDistY = mouseY - outerCircleY;
+  outerCircleX = outerCircleX + (outerCircledDistX * outerCircleSpeed);
+  outerCircleY = outerCircleY + (outerCircledDistY * outerCircleSpeed);
+  outerCircle.style.left = outerCircleX + "px";
+  outerCircle.style.top = outerCircleY + "px";
+  requestAnimationFrame(animateOuterCircle);
+}
+animateOuterCircle();
 
+const ball = document.querySelector("div#Ball");
 let ballX = 0;
 let ballY = 0;
-
-let speed = 0.25;
-
-
-function animate(){
-  
-  let distX = mouseX - ballX;
-  let distY = mouseY - ballY;
-  
-  
-  ballX = ballX + (distX * speed);
-  ballY = ballY + (distY * speed);
-  
+let ballSpeed = 0.8;
+function animateBall(){
+  let ballDistX = mouseX - ballX;
+  let ballDistY = mouseY - ballY;
+  ballX = ballX + (ballDistX * ballSpeed);
+  ballY = ballY + (ballDistY * ballSpeed);
   ball.style.left = ballX + "px";
   ball.style.top = ballY + "px";
-  
-  requestAnimationFrame(animate);
+  requestAnimationFrame(animateBall);
 }
-animate();
+animateBall();
 
 document.addEventListener("mousemove", function(event){
   mouseX = event.pageX;
@@ -60,10 +65,42 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 const ageText = document.getElementById("age");
 
-ageText.onmouseover = function () {
-    ageText.innerHTML = "2009/03/21"
+function loadExternalScript(url, callback) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.onload = callback;
+    document.head.appendChild(script);
 }
-ageText.onmouseout= function () {
-    ageText.innerHTML = calculateAge(birthdate);
-}
+
+loadExternalScript('scramble.js', function() {
+    const currentAge = calculateAge(birthdate);
+    const ageAndBirthday = [currentAge.toString(), "2009/03/21"];
+    const ageText = document.getElementById('age');
+    const ageScrambled = new TextScramble(ageText);
+    let counterAge = 0;
+    const nextAge = () => {
+        ageScrambled.setText(ageAndBirthday[counterAge]).then(() => {
+            setTimeout(() => {
+                counterAge = (counterAge + 1) % ageAndBirthday.length;
+                nextAge();
+            }, 3200);
+        });
+    };
+    nextAge();
+
+    const pasions = ['programming', '3D modeling'];
+    const pasionText = document.getElementById('pasion');
+    const passionScrambled = new TextScramble(pasionText);
+    let counterPassion = 0;
+
+    const nextPassion = () => {
+        passionScrambled.setText(pasions[counterPassion]).then(() => { 
+        setTimeout(nextPassion, 3200);
+    });
+        counterPassion = (counterPassion + 1) % pasions.length;
+    };
+    nextPassion();
+});
+
 
